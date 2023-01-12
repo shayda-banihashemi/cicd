c7217a9824da5b4e01a066f1954d5f4a78e6f26a
Shayda Banihashemi <shayda.banihashemi@gmail.com>
2023-01-11 18:41:33 -0700

    first commit

---
+++ src/cicd/maths.py
@@ -0,0 +1,4 @@
+"""This is a library for general maths functions"""
+def addup(num1: int, num2: int) -> int:
+    """This function adds up two numbers"""
+    return num1 + num2
---
+++ tests/test_maths.py
@@ -0,0 +1,4 @@
+import cicd.maths
+
+def test_addup():
+    assert cicd.maths.addup(3,4) == 7
